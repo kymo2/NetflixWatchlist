5c3ab17d1c36db0873c75ef2c39b7358bed2c1a8
kymo2 <kmoon35@gmail.com>
2025-01-30 09:42:26 -0800

    Initial Commit

---
+++ NetflixWatchlist/NetflixWatchlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixWatchlistApp.swift
+//  NetflixWatchlist
+//
+//  Created by Kyle Mooney on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct NetflixWatchlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
